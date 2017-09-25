ce7fefea26107b0b25acb141a09d61fd1d14df56
Naveen Reddy Gundlagutta <naveen.gundlagutta@nutanix.com>
2017-09-25 16:11:55 +0530

    Dir Layout and Sample Files

---
+++ py/GenerateComponents.py
@@ -0,0 +1,7 @@
+
+class GenerateComponents():
+  def __init__(self):
+    self.one = 1
+
+  def one(self):
+    return 1
---
+++ py/test/TestComponentOne.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from py.GenerateComponents import GenerateComponents
+
+import unittest
+
+class TestComponentOne(unittest.TestCase):
+
+  def test_all(self):
+    self.assertEqual(1, 1)
+
